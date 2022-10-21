1b75684ebab4ee768e0ba197feecac40f1b21407
Shreyansh Singh <92965081+Shreyansh2303@users.noreply.github.com>
2022-10-21 12:22:50 +0530

    Time: 548 ms (8.63%), Space: 77.2 MB (60.05%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> idxMap;
+        
+        int n = nums.size();
+        for (int i=0; i<n; i++) {
+            
+            if (idxMap.find(nums[i]) != idxMap.end() and  i-idxMap[nums[i]] <= k) return true;
+            idxMap[nums[i]] = i;
+        }
+        
+        
+        for (auto el: idxMap) {
+            cout << el.first << ' ' << el.second << '\n';
+        }
+        
+        return false;
+    }
+};
