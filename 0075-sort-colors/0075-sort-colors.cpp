17991f15e07b5d62caaa6e06cd81319735597825
Shreyansh Singh <92965081+Shreyansh2303@users.noreply.github.com>
2022-10-21 18:37:18 +0530

    Time: 3 ms (66.87%), Space: 8.3 MB (70.11%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+        int red = 0, white = 0, blue = 0;
+        
+        for (auto num: nums) {
+            if (num == 0) red++;
+            else if (num == 1) white++;
+            else blue++;
+        }
+        
+        int i=0;
+        while (red--) nums[i++] = 0;
+        while (white--) nums[i++] = 1;
+        while (blue--) nums[i++] = 2;
+    }
+};
